d98da404c27e940450030411436a5dff88ab28df
Dhruvin Patel <104400084+pdhruvin25@users.noreply.github.com>
2023-08-15 13:12:39 -0400

    Updates

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -5,10 +5,10 @@ import {GoBook} from "react-icons/go"
 import {GrContact} from "react-icons/gr"
 
  const Nav = () => {
-  const [activeNav, setActiveNav] = useState("#")
+  const [activeNav, setActiveNav] = useState("#header")
   return (
     <Nav>
-<a href = "#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><HiHome/></a>
+<a href = "#header" onClick={() => setActiveNav("#header")} className={activeNav === "#header" ? "active" : ""}><HiHome/></a>
 <a href = "#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><BsFillPersonLinesFill/></a>
 <a href = "#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><GoBook/></a>
 <a href = "#services"onClick={() => setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><BsGearWideConnected/></a>
