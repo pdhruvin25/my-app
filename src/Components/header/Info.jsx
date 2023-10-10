0382f46b53e1d4ba03d92881b017f67944f21485
Dhruvin Patel <pateldhruvin2503@gmail.com>
2023-10-10 12:45:33 -0400

    Changed resume button to view instead of download

--- src/Components/header/Info.jsx
+++ src/Components/header/Info.jsx
@@ -5,7 +5,7 @@ import CV from "../../Resume_New.pdf"
 const Info = () => {
   return (
     <div className='Info'>
-<a href= {CV} className='btn' target = "_blank">Open Resume</a>
+<a href= {CV} className='btn' target = "blank">Open Resume</a>
 <a href= "#contact" className='btn'>Lets Talk</a>
 
     </div>
